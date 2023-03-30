3c2e5c3e9c9f2c0862b66ad8a33895e35fed0493
Daniel Berger <daniel.berger@covermymeds.com>
2023-03-30 08:58:51 -0400

    Use single quotes.

--- spec/final_spec.rb
+++ spec/final_spec.rb
@@ -18,24 +18,24 @@ RSpec.describe Final do
     @foo = klass.new
   end
 
-  example "version constant returns expected version number" do
+  example 'version constant returns expected version number' do
     expect(Final::FINAL_VERSION).to eq('0.3.1')
     expect(Final::FINAL_VERSION).to be_frozen
   end
 
-  example "final class cannot be subclassed" do
+  example 'final class cannot be subclassed' do
     expect{ class Bar < klass; end }.to raise_error(Final::Error)
   end
 
-  example "final class cannot have instance methods redefined" do
-    expect{ klass.define_method(:bar){ "bar" } }.to raise_error(Final::Error)
+  example 'final class cannot have instance methods redefined' do
+    expect{ klass.define_method(:bar){ 'bar' } }.to raise_error(Final::Error)
   end
 
-  example "adding a new method is not an error" do
-    expect{ klass.define_method(:foo){ "foo" } }.not_to raise_error
+  example 'adding a new method is not an error' do
+    expect{ klass.define_method(:foo){ 'foo' } }.not_to raise_error
   end
 
-  example "finalized classes do not interfere with each other" do
+  example 'finalized classes do not interfere with each other' do
     expect{ class Foo2; include Final; def bar; end; end }.not_to raise_error
   end
 end
